5fd832287416df91ab3ad89f95999901283a9264
AB-Siddique278 <absiddique1060@gmail.com>
2022-05-21 22:55:03 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import MyPortfolio from './Pages/MyPortfolio/MyPortfolio';
+import Login from './Pages/Login/Login';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div>
-      <button class="btn btn-success">Success</button>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="myPortfolio" element={<MyPortfolio />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
