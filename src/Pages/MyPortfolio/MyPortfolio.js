d1e39732af39ce6f011adf88c3aad0431a6b1868
AB-Siddique278 <absiddique1060@gmail.com>
2022-05-24 07:11:20 +0600

    make my portfolio

--- src/Pages/MyPortfolio/MyPortfolio.js
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -3,7 +3,49 @@ import React from 'react';
 const MyPortfolio = () => {
     return (
         <div>
-            <h1> this is my protfolio</h1>
+            <div>
+                <div class="hero  lg:h-[80vh] ">
+                    <div class="hero-content flex-col lg:flex-row-reverse">
+                     
+
+
+
+                        <div class="card  bg-base-100 ">
+                            <figure class="px-10 pt-10">
+                                <img src="https://i.ibb.co/bKJztHf/Abu-Bakar-Siddique.png" alt="Shoes" class="rounded-xl" />
+                            </figure>
+                          
+                        </div>
+
+
+
+
+
+
+
+                        <div className=' max-w-3xl'>
+                            <h1 class="text-5xl font-bold">HELLO' THIS IS <br />ABU BAKAR SIDDIWUE <br /><span className=' text-3xl'>Expert in Development</span> </h1>
+                            <p class="py-6 text-xl font-mono ">I am Full stack Web developer. I am expert in HTML, CSS, JAVA-Script, Flutter,c, c++, Python PHP with WordPress. <b/> Now I am working in the Fiverr marketplace, And My hobby is photography.</p>
+                            <p className='text-xl font-mono '>I am studying in Computer Science and Engineering Department at Daffodil International University. </p>
+                            <br />
+                            <p className='text-xl font-mono '>My permanent address is Barisal Bhola Charfashion. And present address Dhaka Dhanmondi 32.</p>
+                             
+                              
+                              <br />
+                              <br />
+                             <p className='text-3xl font-bold'> My Three Project </p>
+                             <a href="https://modest-wing-f91ca5.netlify.app/" target="_blank"> <span className='text-xl text-orange-500'><p> <li>Projec-1</li></p></span></a>
+                             <a href="https://ab-siddique278.github.io/Lab-report-2/" target="_blank"> <span className='text-xl text-orange-500'><p> <li>Projec-2</li></p></span></a>
+                             <a href="https://fruits-basket-b1fcb.web.app/" target="_blank"> <span className='text-xl text-orange-500'><p> <li>Projec-3</li></p></span></a>
+
+
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
