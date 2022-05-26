5c5dd11a765618ad009f8a566b0610c67c487fde
AB-Siddique278 <absiddique1060@gmail.com>
2022-05-26 20:53:33 +0600

    somthing

--- src/Pages/Shared/Loading.js
+++ src/Pages/Shared/Loading.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        // <div className='h-screen flex justify-center items-center'>
-        //    <button class="btn loading">loading</button>
-
-        // </div>
+       
         <div class="h-screen flex justify-center items-center space-x-2 animate-bounce">
             <div class="w-8 h-8 bg-blue-400 rounded-full"></div>
             <div class="w-8 h-8 bg-green-400 rounded-full"></div>
